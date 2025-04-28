15d31a89af4095745b3f5d88e4c3c81a9b882f82
Alex Dontsov <45141221+somvy@users.noreply.github.com>
2025-04-28 16:49:26 +0300

    add mosh to setup

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ chsh -s /bin/bash
 # install oh my bash
 curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh | bash
 
-
+# install mosh
+sudo apt-get install mosh
 
 if ![ -f ~/.bashrc ]; then
     echo "source ~/.bash_aliases" >> ~/.bashrc
